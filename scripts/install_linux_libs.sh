8a59dfb9f1780f12b8f2f4588264d08b7afc0537
Tim Nonet <tim.nonet@gmail.com>
2022-05-24 08:31:42 -0500

    Fix BLASH install scripts

--- scripts/install_linux_libs.sh
+++ scripts/install_linux_libs.sh
@@ -3,13 +3,8 @@
 # Based on https://github.com/mlpack/mlpack-wheels
 # Based on https://gitlab.com/jason-rumengan/pyarma
 
-basedir=$(python3 lib/openblas_support.py)
+basedir=$(python3 scripts/openblas_support.py)
 $use_sudo cp -r $basedir/lib/* /usr/local/lib
 $use_sudo cp $basedir/include/* /usr/local/include
-
-basedir=$(python3 lib/hdf5_support.py)
-$use_sudo cp -r $basedir/lib/* /usr/local/lib
-$use_sudo cp $basedir/include/* /usr/local/include
-$use_sudo cp $basedir/bin/* /usr/local/bin
 
 export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/lib/
