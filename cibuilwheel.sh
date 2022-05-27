2a9df2248d893c29462c630ff6e16fbb24a2e7bb
Tim Nonet <tim.nonet@gmail.com>
2022-05-27 07:28:30 -0500

    Try on windows

--- cibuilwheel.sh
+++ cibuilwheel.sh
@@ -2,7 +2,8 @@ python -m pip install cibuildwheel==2.5.0
 
 export CIBW_SKIP="pp* *-win32"
 export CIBW_BEFORE_ALL_LINUX="yum install -y lapack-devel && bash scripts/install_linux_libs.sh"
+export CIBW_BEFORE_BUILD_WINDOWS="pip install scipy && bash scripts/install_linux_libs.sh"
 export CIBW_BEFORE_TEST="pip install pytest numpy"
 export CIBW_TEST_COMMAND="pytest {package}/tests"
 export CIBW_BUILD_VERBOSITY=3
-python -m cibuildwheel --output-dir wheelhouse --platform linux
+python -m cibuildwheel --output-dir wheelhouse --platform windows --archs AMD64
